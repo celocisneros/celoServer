9d19eabd1a08f2565dc25758331974a770033ecb
celocisneros <celo.cisneros@icloud.com>
2025-07-19 14:31:46 +0000

    created serverBox to show message from the backend

---
+++ public/main.js
@@ -0,0 +1,9 @@
+const { response } = require("express");
+
+function message(){
+    fetch('/api/message') //calls the backend route
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('serverBox').textContent = data.message;
+    })
+};
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+// app.get, the route, the callback function, the data
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'hello from the backend'});
+});
+
+
 
 // Listen on port 3000
 const PORT = 3000;
