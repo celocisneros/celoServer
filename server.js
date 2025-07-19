de02aaf4d326f55004b3868e175ca860cd91454a
celocisneros <celo.cisneros@icloud.com>
2025-07-19 15:35:41 +0000

    order of server js

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// app.get, the route, the callback function, the data
+
+app.get('/api/message', (req, res) => {
+    console.log('✅ GET /api/message was called');
+    res.json({ message: 'hello from the backend' });
+});
+
 // Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Default route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// app.get, the route, the callback function, the data
-
-app.get('/api/message', (req, res) => {
-    console.log('✅ GET /api/message was called');
-    res.json({ message: 'hello from the backend' });
 });
 
 
