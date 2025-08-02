790804e23fce095d45d488d0d460470318767bc4
celocisneros <celo.cisneros@icloud.com>
2025-08-02 18:51:13 +0000

    fixed a line after successful login trying to get the redirect to profile set up

--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,6 @@ router.post('/', (req, res) => {
   const { username, password } = req.body;
 
   if (username === dummyUser.username && password === dummyUser.password) {
-    res.send('Login successful!');
     res.redirect('/profile.html');
   } else {
     res.status(401).send('Invalid credentials.');
