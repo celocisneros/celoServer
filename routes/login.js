7bc125f90c3c361f1a39adf33ce57d3b742b83c3
celocisneros <celo.cisneros@icloud.com>
2025-08-03 00:03:38 +0000

    serious commit

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,7 @@ router.post('/', (req, res) => {
   const { username, password } = req.body;
 
   if (username === dummyUser.username && password === dummyUser.password) {
+    req.session.username = username; // MUST be set before redirect
     res.redirect('/dashboard.html');
   } else {
     res.status(401).send('Invalid credentials.');
--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.get('/api/user', (req, res) => {
 const loginRoute = require('./routes/login');
 app.use('/login', loginRoute);
 
+
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
