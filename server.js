d43685facb04130e9d9a67512e2aa885dcba0def
celocisneros <celo.cisneros@icloud.com>
2025-08-10 09:29:48 +0000

    added 2 lines to connect the auth js in my routes directory to the app.js file

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ mongoose.connect('mongodb://localhost:27017/abyssportal', {
 .then(() => console.log('MongoDB connected'))       // If successful, log this
 .catch(err => console.error('MongoDB error:', err)); // If there's an error, log it
 
+// Import routes
+const authRoutes = require('./routes/auth');
+
+// Use routes with /api prefix
+app.use('/api', authRoutes);
+
 // Test route to make sure the API is working
 app.get('/api/test', (req, res) => {
   res.send('API is working!'); // Responds with text when someone visits /api/test
